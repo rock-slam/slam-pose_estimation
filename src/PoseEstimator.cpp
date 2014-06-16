46e02042d522f30bb35927a13ee5b743403706b3
Sascha Arnold <sascha.arnold@dfki.de>
2014-06-16 17:23:43 +0200

    added a pose estimator class which can have different types of filters

---
+++ src/PoseEstimator.cpp
@@ -0,0 +1,130 @@
+#include "PoseEstimator.hpp"
+#include <pose_estimation/PoseEKF.hpp>
+#include <base/Logging.hpp>
+#include <base/Float.hpp>
+
+namespace pose_estimation
+{
+
+PoseEstimator::PoseEstimator() : last_measurement_time(base::Time::fromSeconds(0.0)), max_time_delta(base::infinity<double>())
+{
+    //FIXME select pose ekf for now
+    filter.reset(new PoseEKF());
+}
+
+void PoseEstimator::setInitialState(const base::samples::RigidBodyState& body_state)
+{
+    filter->setInitialState(body_state);
+}
+
+void PoseEstimator::setProcessNoise(const Covariance& process_noise)
+{
+    filter->setProcessNoiseCovariance(process_noise);
+}
+
+void PoseEstimator::setMaxTimeDelta(double max_time_delta)
+{
+    this->max_time_delta = max_time_delta;
+}
+
+bool PoseEstimator::enqueueMeasurement(const base::samples::RigidBodyState& body_state, const Measurement::MemberMask& member_mask)
+{
+    if(!checkMemberMask(member_mask))
+    {
+	throw std::runtime_error("Member mask contains invalid values. Only 0 and 1 is allowed." );
+    }
+    
+    Measurement measurement;
+    measurement.body_state = body_state;
+    measurement.member_mask = member_mask;
+    return enqueueMeasurement(measurement);
+}
+
+bool PoseEstimator::enqueueMeasurement(const Measurement& measurement)
+{
+    if(measurement.body_state.time < last_measurement_time)
+	return false;
+    
+    measurement_queue.push(measurement);
+    
+    return true;
+}
+
+void PoseEstimator::integrateMeasurements()
+{
+    while (!measurement_queue.empty())
+    {
+	Measurement measurement = measurement_queue.top();
+	measurement_queue.pop();
+
+	processMeasurement(measurement);
+    }
+}
+
+void PoseEstimator::integrateMeasurements(const base::Time& current_time)
+{
+    integrateMeasurements();
+    
+    if(!last_measurement_time.isNull())
+    {
+	double time_delta = (current_time - last_measurement_time).toSeconds();
+	
+	if(time_delta < 0.0)
+	    throw std::runtime_error("Attempt to go back in time. Time delta is negative!");
+	
+	// prediction step
+	if(time_delta > max_time_delta)
+	{ 
+	    LOG_WARN("Time delta is to high: %f. Skip this prediction step.");
+	}
+	else if(time_delta > 0.0)
+	    filter->predictionStep(time_delta);
+    }
+    
+    last_measurement_time = current_time;
+}
+
+void PoseEstimator::processMeasurement(const Measurement& measurement)
+{
+    if(!last_measurement_time.isNull())
+    {
+	double time_delta = (measurement.body_state.time - last_measurement_time).toSeconds();
+	
+	if(time_delta < 0.0)
+	    throw std::runtime_error("Attempt to process an older measurement. Time delta is negative!");
+	
+	// prediction step
+	if(time_delta > max_time_delta)
+	{ 
+	    LOG_WARN("Time delta is to high: %f. Skip this prediction step.");
+	}
+	else if(time_delta > 0.0)
+	    filter->predictionStep(time_delta);
+    }
+    
+    // correction step
+    filter->correctionStep(measurement);
+    
+    last_measurement_time = measurement.body_state.time;
+}
+
+base::samples::RigidBodyState PoseEstimator::getEstimatedState()
+{
+    base::samples::RigidBodyState state = filter->getCurrentState();
+    state.time = last_measurement_time;
+    return state;
+}
+
+bool PoseEstimator::checkMemberMask(const Measurement::MemberMask& member_mask)
+{
+    if(member_mask.rows() != BODY_STATE_SIZE || member_mask.cols() != 1)
+	return false;
+    for(unsigned i = 0; i < BODY_STATE_SIZE; i++)
+    {
+	if(!(member_mask(i, 0) == 0 || member_mask(i, 0) == 1))
+	    return false;
+    }
+    return true;
+}
+
+}
---
+++ src/PoseEstimator.hpp
@@ -0,0 +1,47 @@
+#ifndef _POSE_ESTIMATION_POSE_ESTIMATOR_HPP
+#define _POSE_ESTIMATION_POSE_ESTIMATOR_HPP
+
+#include <pose_estimation/Measurement.hpp>
+#include <pose_estimation/AbstractFilter.hpp>
+
+#include <boost/shared_ptr.hpp>
+#include <queue>
+
+namespace pose_estimation
+{
+
+class PoseEstimator
+{
+public:
+    PoseEstimator();
+    
+    void setInitialState(const base::samples::RigidBodyState &body_state);
+    void setProcessNoise(const Covariance& process_noise);
+    void setMaxTimeDelta(double max_time_delta);
+    
+    bool enqueueMeasurement(const base::samples::RigidBodyState& body_state,
+			    const Measurement::MemberMask& member_mask);
+    bool enqueueMeasurement(const Measurement& measurement);
+    
+    void integrateMeasurements();
+    void integrateMeasurements(const base::Time& current_time);
+    
+    void processMeasurement(const Measurement &measurement);
+    
+    base::samples::RigidBodyState getEstimatedState();
+    
+    
+protected:
+    bool checkMemberMask(const Measurement::MemberMask& member_mask);
+    
+    
+    std::priority_queue<Measurement, std::vector<Measurement>, Measurement> measurement_queue;
+    boost::shared_ptr<AbstractFilter> filter;
+    base::Time last_measurement_time;
+    double max_time_delta;
+};
+
+}
+
+
+#endif
