034558fbfd7f4a9c40a46bcf659967b8126bc0b1
Sascha Arnold <sascha.arnold@dfki.de>
2015-09-03 19:07:01 +0200

    Allow real partial updates on the UKF filter.
    Improves integration speed and solves a potential numeric problem.
    This is based on making the MTK UKF filter understand dynamic
    sized measurements.

--- src/mtk_ukf/MeasurementModels.hpp
+++ src/mtk_ukf/MeasurementModels.hpp
@@ -1,18 +1,19 @@
 #ifndef _MEASUREMENT_MODELS_HPP_
 #define _MEASUREMENT_MODELS_HPP_
 
-#include "PoseWithVelocity.hpp"
+#include <Eigen/Core>
 
 namespace pose_estimation
 {
-    
+
 /** Measurement model for the 12D robot state (pose and velocity)
+ *  The mask defines which subset is returned.
  */
 template <typename PoseWithVelocityType>
-PoseWithVelocityType
-measurementModel (const PoseWithVelocityType &state)
+Eigen::Matrix<typename PoseWithVelocityType::scalar, -1, 1>
+measurementModel (const PoseWithVelocityType &state, const Eigen::Matrix<unsigned, PoseWithVelocityType::DOF, 1>& mask)
 {
-    return state;
+    return state.getSubStateVector(mask);
 }
     
 }
