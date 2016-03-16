150adcd3c606af9c2649f2ef380a9e5ac83aeb4a
Lashika Medagoda <Lashika.Medagoda@dfki.de>
2016-03-16 13:13:49 +0100

    added OrientationState.hpp

---
+++ src/orientation_estimator/OrientationState.hpp
@@ -0,0 +1,39 @@
+#ifndef _ORIENTATION_STATE_HPP_
+#define _ORIENTATION_STATE_HPP_
+
+/** MTK library **/
+#include <mtk/src/SubManifold.hpp>
+#include <mtk/types/SOn.hpp>
+#include <mtk/types/vect.hpp>
+#include <mtk/build_manifold.hpp>
+#include <mtk/startIdx.hpp>
+#include <boost/concept_check.hpp>
+#include <pose_estimation/ManifoldHelper.hpp>
+#include <Eigen/Core>
+
+namespace pose_estimation
+{
+
+typedef MTK::SO3<double> RotationType;
+typedef RotationType::vect_type TranslationType;
+typedef RotationType::vect_type VelocityType;
+
+MTK_BUILD_MANIFOLD(OrientationState,
+   ((RotationType, orientation))
+   ((VelocityType, velocity)) // navigation/target frame velocity
+   ((VelocityType, bias_gyro))
+   ((VelocityType, bias_acc))
+)
+
+template<>
+inline void getStateVector(const OrientationState &state, Eigen::Matrix<OrientationState::scalar, OrientationState::DOF, 1>& state_vector)
+{
+    state_vector.block(MTK::getStartIdx(&OrientationState::orientation),0,MTK::getDof(&OrientationState::orientation),1) = MTK::SO3<OrientationState::scalar>::log(state.orientation);
+    state_vector.block(MTK::getStartIdx(&OrientationState::velocity),0,MTK::getDof(&OrientationState::velocity),1) = state.velocity;
+    state_vector.block(MTK::getStartIdx(&OrientationState::bias_gyro),0,MTK::getDof(&OrientationState::bias_gyro),1) = state.bias_gyro;
+    state_vector.block(MTK::getStartIdx(&OrientationState::bias_acc),0,MTK::getDof(&OrientationState::bias_acc),1) = state.bias_acc;    
+}
+
+}
+
+#endif
